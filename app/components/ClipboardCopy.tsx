052c57c0b85168ac00d69fb243a589a65c5593b8
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-14 01:33:50 +0000

    bugfix

--- app/components/ClipboardCopy.tsx
+++ app/components/ClipboardCopy.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-const ClipboardCopy = ({defaultText= "The sun was setting behind the mountains, casting a golden glow over the quiet village. The air was cool and crisp, filled with the scent of pine trees and fresh earth. As the villagers went about their evening routines, there was a sense of peace in the air, as if the world had slowed down for just a moment."} : {defaultText:string}) => {
+const ClipboardCopy = ({ defaultText = "The sun was setting behind the mountains, casting a golden glow over the quiet village. The air was cool and crisp, filled with the scent of pine trees and fresh earth. As the villagers went about their evening routines, there was a sense of peace in the air, as if the world had slowed down for just a moment." }: { defaultText: string }) => {
   const [input, setInput] = useState("");
   const [copied, setCopied] = useState(false); // Copied! の状態
 
 
-    ;
+  ;
 
   const handleCopy = () => {
     const textToCopy = input || defaultText;
@@ -55,8 +55,8 @@ const ClipboardCopy = ({defaultText= "The sun was setting behind the mountains, 
           className="absolute right-0 bottom-4 mb-1 mr-4 w-6 h6 flex items-center justify-center"
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 stroke-current text-slate-800 dark:text-white" viewBox="0 0 24 24">
-          <path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 20h9M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854zM15 5l3 3"/>
-        </svg>
+            <path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 20h9M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854zM15 5l3 3" />
+          </svg>
 
         </button>
       </form>
